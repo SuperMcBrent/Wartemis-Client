bad4dec609b24c46d369d81f3437eb81fdea6fe3
Brent Sagaert <brentsagaert@gmail.com>
2021-02-25 12:33:08 +0100

    Queen Evaluate

--- WartemisBots/ChessBot/Models/Pieces/Queen.cs
+++ WartemisBots/ChessBot/Models/Pieces/Queen.cs
@@ -9,7 +9,131 @@ namespace Chess.Models.Pieces {
         }
 
         public override void Evaluate() {
-            throw new NotImplementedException();
+            Cell cell;
+
+            #region Cardinal Movement
+            // go north until end of board or piece is blocking, if blocking piece is enemy add to endangering
+            cell = Cell;
+            do {
+                cell = cell.CellToNorth();
+                if (cell is null) break;
+
+                AddToAvailableOrUnavailableTargets(cell);
+
+                if (!cell.IsEmpty()) {
+                    AddToProtectingOrEndangeringPieces(cell);
+                    break;
+                }
+
+            } while (cell != null);
+
+            // go east until end of board or piece is blocking, if blocking piece is enemy add to endangering
+            cell = Cell;
+            do {
+                cell = cell.CellToEast();
+                if (cell is null) break;
+
+                AddToAvailableOrUnavailableTargets(cell);
+
+                if (!cell.IsEmpty()) {
+                    AddToProtectingOrEndangeringPieces(cell);
+                    break;
+                }
+
+            } while (cell != null);
+
+            // go south until end of board or piece is blocking, if blocking piece is enemy add to endangering
+            cell = Cell;
+            do {
+                cell = cell.CellToSouth();
+                if (cell is null) break;
+
+                AddToAvailableOrUnavailableTargets(cell);
+
+                if (!cell.IsEmpty()) {
+                    AddToProtectingOrEndangeringPieces(cell);
+                    break;
+                }
+
+            } while (cell != null);
+
+            // go west until end of board or piece is blocking, if blocking piece is enemy add to endangering
+            cell = Cell;
+            do {
+                cell = cell.CellToWest();
+                if (cell is null) break;
+
+                AddToAvailableOrUnavailableTargets(cell);
+
+                if (!cell.IsEmpty()) {
+                    AddToProtectingOrEndangeringPieces(cell);
+                    break;
+                }
+
+            } while (cell != null);
+            #endregion
+
+            #region Diagonal Movement
+            // go northeast until end of board or piece is blocking, if blocking piece is enemy add to endangering
+            cell = Cell;
+            do {
+                cell = cell.CellToNorthEast();
+                if (cell is null) break;
+
+                AddToAvailableOrUnavailableTargets(cell);
+
+                if (!cell.IsEmpty()) {
+                    AddToProtectingOrEndangeringPieces(cell);
+                    break;
+                }
+
+            } while (cell != null);
+
+            // go southeast until end of board or piece is blocking, if blocking piece is enemy add to endangering
+            cell = Cell;
+            do {
+                cell = cell.CellToSouthEast();
+                if (cell is null) break;
+
+                AddToAvailableOrUnavailableTargets(cell);
+
+                if (!cell.IsEmpty()) {
+                    AddToProtectingOrEndangeringPieces(cell);
+                    break;
+                }
+
+            } while (cell != null);
+
+            // go southwest until end of board or piece is blocking, if blocking piece is enemy add to endangering
+            cell = Cell;
+            do {
+                cell = cell.CellToSouthWest();
+                if (cell is null) break;
+
+                AddToAvailableOrUnavailableTargets(cell);
+
+                if (!cell.IsEmpty()) {
+                    AddToProtectingOrEndangeringPieces(cell);
+                    break;
+                }
+
+            } while (cell != null);
+
+            // go northwest until end of board or piece is blocking, if blocking piece is enemy add to endangering
+            cell = Cell;
+            do {
+                cell = cell.CellToNorthWest();
+                if (cell is null) break;
+
+                AddToAvailableOrUnavailableTargets(cell);
+
+                if (!cell.IsEmpty()) {
+                    AddToProtectingOrEndangeringPieces(cell);
+                    break;
+                }
+
+            } while (cell != null);
+            #endregion
         }
     }
 }
