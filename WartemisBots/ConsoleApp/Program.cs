d777934deb4ada255a669b7193ccd5fd5240ab25
SuperMcBrent <brentsagaert@gmail.com>
2021-03-03 16:22:08 +0100

    name

--- WartemisBots/ConsoleApp/Program.cs
+++ WartemisBots/ConsoleApp/Program.cs
@@ -25,7 +25,7 @@ namespace ConsoleApp {
             };
             #endregion
 
-            ChessBot cb = new ChessBot(name: "Chesstily");
+            ChessBot cb = new ChessBot(name: "<u>Chesstily</u>");
             cb.Start();
 
             cb.Connection.RaiseOnStateReceivedEventManually(
